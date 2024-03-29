64201559e19664407c620a56c27beca053a98ea0
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-03-29 22:40:26 +0530

    update2

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,11 @@
-console.log("Hello worls"); 
-console.log("Hello worls new");
-console.log("Hello worls nefgfgfw");
+const express=require("express");
+const cors=require("cors");
+const axios=require("axios");
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(5000,()=>{
+    console.log("SERVER STARTED0");
+});
